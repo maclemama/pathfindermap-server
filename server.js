7feec74df3b230b53d2a76c1d330b2176d83c594
maclemama <maclema@me.com>
2023-08-23 14:08:09 +0100

    inital commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const ENV = process.env.ENV;
+const PORT = ENV === "PROD" ? "" : process.env.PORT || 8080;
+const HOST = ENV === "PROD" ? process.env.PROD_HOST : process.env.DEV_HOST;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${HOST}${ENV === "DEV" ? ":" : ""}${PORT}`);
+});
